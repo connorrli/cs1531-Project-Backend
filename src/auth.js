8c852900a85119c954d1b582a6cfee7d753fcf7d
Connor Li <z5425430@ad.unsw.edu.au>
2024-02-20 22:14:27 +1100

    Added adminAuthLogin function stub to auth.js
    Will be added to in future iterations.

--- src/auth.js
+++ src/auth.js
@@ -7,3 +7,9 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
         authUserId: 1
     };
 }
+
+function adminAuthLogin(email, password) {
+    return {
+        authUserId: 1
+    };
+}
