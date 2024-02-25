2cf781448005399c4d06b987c50221ae6aa58fc8
Connor Li <z5425430@ad.unsw.edu.au>
2024-02-26 03:50:51 +1100

    Added an export line in other.js for clear()
    Required to run all tests

--- src/other.js
+++ src/other.js
@@ -2,9 +2,11 @@
   * Resets the state of the application back to the start. This function does not take in any parameters or return anything.
 */
 
-import { getData, setData } from './datastore.js';
+import { getData, setData } from './dataStore.js';
 
 function clear() {
   setData({ users: [], quizzes: [] });
   return {};
 }
+
+export { clear };
