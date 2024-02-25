0e3021332b9fd0f2c26b2c3eb25f48db56025024
Connor Li <z5425430@ad.unsw.edu.au>
2024-02-26 04:01:14 +1100

    Revert "Merge branch 'connor/otherjs-export' into 'master'"
    This reverts merge request !46

--- src/other.js
+++ src/other.js
@@ -2,11 +2,9 @@
   * Resets the state of the application back to the start. This function does not take in any parameters or return anything.
 */
 
-import { getData, setData } from './dataStore.js';
+import { getData, setData } from './datastore.js';
 
 function clear() {
   setData({ users: [], quizzes: [] });
   return {};
 }
-
-export { clear };
