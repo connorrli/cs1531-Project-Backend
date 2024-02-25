4f8305ae751c1d522b4f59dde93ec616739d79de
Zhitan Wu <z5600055@ad.unsw.edu.au>
2024-02-25 21:16:26 +1100

    Updated other.js

--- src/other.js
+++ src/other.js
@@ -1,6 +1,9 @@
 /**
   * Resets the state of the application back to the start. This function does not take in any parameters or return anything.
 */
+
+import { getData, setData } from './datastore.js';
+
 function clear() {
-    return {};
+  return setData({ users: [], quizzes: [] });
 }
