6254b3c4b04eca38fc8b170ee4770cba1b430303
Zhitan Wu <z5600055@ad.unsw.edu.au>
2024-02-27 16:50:07 +1100

    New File For Debugging

---
+++ src/tests/other_tests_.js
@@ -0,0 +1,35 @@
+import { clear } from '../other.js';
+import { getData, setData } from './dataStore.js';
+
+beforeEach(() => {
+    // Set up some initial data before each test
+    setData({ users: [{ id: 1, name: 'John' }], quizzes: [{ id: 1, title: 'Math Quiz' }] });
+});
+
+test('Should clear user data', () => {
+    // Call clear function
+    clear();
+
+    // Assert that user data is cleared
+    const userData = getData('users');
+    expect(userData).toEqual([]);
+});
+
+test('Should clear quiz data', () => {
+    // Call clear function
+    clear();
+
+    // Assert that quiz data is cleared
+    const quizData = getData('quizzes');
+    expect(quizData).toEqual([]);
+});
+
+test('Should not affect other data in the store', () => {
+    // Call clear function
+    clear();
+
+    // Assert that other data in the store remains unchanged
+    const otherData = getData('someOtherData');
+    // Add assertions based on the expected behavior of your data store
+    // For example, you might check that otherData is still as expected
+});
