a3822a6db62910ce3833885cffe89adf3988aaa4
Connor Li <z5425430@ad.unsw.edu.au>
2024-02-29 02:24:35 +1100

    Updated the adminUserDetailsUpdate error checks
    Correctly handles all error checking

--- src/helpers/userUpdateErrors.js
+++ src/helpers/userUpdateErrors.js
@@ -1,15 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// IMPORTS /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
 import { checkForErrors } from './checkForErrors.js';
+import { getData } from '../dataStore.js';
+
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// CONSTANTS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 
 const NO_ERROR = 0;
 
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// FUNCTIONS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+/**
+  * Check updated details of a user for any errors
+  * 
+  * @param {integer} authUserId - The user's unique id
+  * @param {integer} email - The user's current or updated email
+  * @param {string} nameFirst - The user's current or updated first name
+  * @param {string} nameLast - The user's current or updated last name
+  * 
+  * @returns {error object or NO_ERROR} - Returns either an error object or NO_ERROR (0)
+*/
 function checkDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+    const data = getData();
     const checks = new Map([
         [authUserId, ['authUserIdValid']],
-        [email, ['emailValid', 'emailInUse']], 
+        [email, ['emailValid']], 
         [nameFirst, ['nameFirstValid']],
-        [nameLast, ['nameLastValid']]
+        [nameLast, ['nameLastValid']],
     ]);
 
+    // Only check email is in use if it's not the user's current email
+    const theUser = data['users'].find(user => user.userId === authUserId);
+    if (theUser !== undefined) {
+        if (theUser['email'] !== email) {
+            checks.set(email, ['emailValid', 'emailInUse']);
+        }
+    }
+
+    // Do all checks mapped to each user detail
     for (const [key, value] of checks.entries()) {
         for (const check of value) {
             let error = checkForErrors(key, check);
@@ -18,6 +51,10 @@ function checkDetailsUpdate(authUserId, email, nameFirst, nameLast) {
     }
     return NO_ERROR;
 }
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// EXPORTS /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 
 export { checkDetailsUpdate };
 
