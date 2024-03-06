fbe2a8d1bf2abbcd479a205bfa9ebdb282a1d6b9
Holly Shephard <z5476845@ad.unsw.edu.au>
2024-03-06 12:39:14 +1100

    added some more tests

--- src/tests/auth/adminUserDetails.test.js
+++ src/tests/auth/adminUserDetails.test.js
@@ -12,7 +12,14 @@ beforeEach(() => {
 });
 
 const ERROR = { error: expect.any(String) };
-
+const USER = { user: { 
+        userId: expect.any(Number),
+        name: expect.any(String),
+        email: expect.any(String),
+        numSuccessfulLogins: expect.any(Number),
+        numFailedPasswordsSinceLastLogin: expect.any(Number)
+    }
+};
 
 describe('Should give correct user details', () => {
     test('Correct user details', () => {
@@ -26,9 +33,9 @@ describe('Should give correct user details', () => {
                 numFailedPasswordsSinceLastLogin: 0
         }});
         adminAuthLogin('yabbadabbadoo@gmail.com', 'yabba123dabbA');
-        expect(adminUserDetails(user1.authUserId).numSuccessfulLogins).toEqual(2);
+        expect(adminUserDetails(user1.authUserId).user.numSuccessfulLogins).toEqual(2);
         adminAuthLogin('yabbadabbadoo@gmail.com', 'password123');
-        expect(adminUserDetails(user1.authUserId).numFailedPasswordsSinceLastLogin).toEqual(1);
+        expect(adminUserDetails(user1.authUserId).user.numFailedPasswordsSinceLastLogin).toEqual(1);
         const user2 = adminAuthRegister('johnnymcjohn@gmail.com', 'John9090', 'John', 'Mc-John');
         expect(adminUserDetails(user2.authUserId)).toStrictEqual({
             user: {
@@ -40,6 +47,15 @@ describe('Should give correct user details', () => {
             }
         });
     });
+    test.each( [
+        { email: "hehehoho@gmail.com", password: "HEHEhohoh1234", nameFirst: "hehe", nameLast: "hoho" },
+        { email: "waaahwahhhawh@gmail.com", password: "HuruhurEhohoh1234", nameFirst: "HHhhe", nameLast: "gghuu" },
+        { email: "hotmailbestmail@gmail.com", password: "MIAMI89maim", nameFirst: "Gigamesh", nameLast: "Eater of Worlds" },
+        { email: "icantdothisanymore@gmail.com", password: "iHateJest2024", nameFirst: "Grug", nameLast: "McGreg" }
+    ])('Should give a user for various different registrations', ({ email, password, nameFirst, nameLast }) => {
+        const user = adminAuthRegister(email, password, nameFirst, nameLast);
+        expect(adminUserDetails(user.authUserId)).toStrictEqual(USER);
+    })
 });
 
 describe('Should throw error when needed', () => {
