e7ec216edf74e54434f8f2cca876155add53a90c
Holly Shephard <z5476845@ad.unsw.edu.au>
2024-03-07 21:37:19 +1100

    changed tests so they dont always fail in adminQuizCreat.test.js

--- src/tests/adminQuizCreate.test.js
+++ src/tests/adminQuizCreate.test.js
@@ -21,43 +21,42 @@ describe('Testing QuizCreate function:', () => {
 
     //Invalid character in name
     test('Name contains an invalid character', () => {
-        const { authUserId } = adminAuthRegister('test@egmail.com', 'password', 'Walt', 'Smith');
+        const { authUserId } = adminAuthRegister('test@egmail.com', 'password123', 'Walt', 'Smith');
         quiz1 = adminQuizCreate(authUserId, '@@@@', 'simple description');
         expect(quiz1).toEqual({ error: 'Name contains an invalid character' });
     });
 
     test('Name contains an invalid character', () => {
-        const { authUserId } = adminAuthRegister('test@egmail.com', 'password', 'Walt', 'Smith');
+        const { authUserId } = adminAuthRegister('test@egmail.com', 'password123', 'Walt', 'Smith');
         quiz1 = adminQuizCreate(authUserId, 'Johnny@1', 'simple description');
         expect(quiz1).toEqual({ error: 'Name contains an invalid character' });
     });
 
     // Quiz name < 3 or > 30
     test('Name < 3 characters', () => {
-        const { authUserId } = adminAuthRegister('test@egmail.com', 'password', 'Walt', 'Smith');
+        const { authUserId } = adminAuthRegister('test@egmail.com', 'password123', 'Walt', 'Smith');
         quiz1 = adminQuizCreate(authUserId, 'qu', 'simple description');
         expect(quiz1).toEqual({ error: 'Quiz name is < 3 characters' });
     });
 
     test('Name > 30 characters', () => {
-        const { authUserId } = adminAuthRegister('test@egmail.com', 'password', 'Walt', 'Smith');
+        const { authUserId } = adminAuthRegister('test@egmail.com', 'password123', 'Walt', 'Smith');
         quiz1 = adminQuizCreate(authUserId, 'quizname that would be more than 30 characters long', 'simple description');
         expect(quiz1).toEqual({ error: 'Quiz name is > 30 characters' });
     });
 
     // Quiz description > 100
     test('Description > 100 characters', () => {
-        const { authUserId } = adminAuthRegister('test@egmail.com', 'password', 'Walt', 'Smith');
+        const { authUserId } = adminAuthRegister('test@egmail.com', 'password123', 'Walt', 'Smith');
         quiz1 = adminQuizCreate(authUserId, 'quizname', 'The inexorable march of technological advancement continues unabated, revolutionizing industries, reshaping economies, and fundamentally altering the way we live, work, and interact with the world around us.');
         expect(quiz1).toEqual({ error: 'Quiz description is > 100 characters' });
     });
 
     // Quiz name already in use
     test('Quiz name is already in use', () => {
-        let authUserId1 = adminAuthRegister('123@email.com', '1234qwe', 'test', 'ting');
-        let authUserId2 = adminAuthRegister('123456@email.com', '1234qwe456', 'test6', '6ting');
-        quiz1 = adminQuizCreate(authUserId1, 'samename', 'simple description');
-        quiz2 = adminQuizCreate(authUserId2, 'samename', 'description');
+        let authUserId1 = adminAuthRegister('123@email.com', '1234qwef', 'test', 'ting');
+        quiz1 = adminQuizCreate(authUserId1.authUserId, 'samename', 'simple description');
+        quiz2 = adminQuizCreate(authUserId1.authUserId, 'samename', 'description');
         expect(quiz2).toStrictEqual({ error: 'Quiz name is already in use' });
     })
 });
