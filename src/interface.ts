7a8a0eee4a67930cde679ba35a484ad00fd0c732
Connor Li <z5425430@ad.unsw.edu.au>
2024-03-17 00:48:11 +1100

    Added a new global interface file, interface.ts
    This will be used for keeping all universal
    interfaces in the one spot, and can be imported
    freely within other files.

---
+++ src/interface.ts
@@ -0,0 +1,46 @@
+// BELOW THIS WILL BE ALL THE UNIVERSALLY REQUIRED TYPESCRIPT INTERFACES
+// ANY LOCAL-SCOPE INTERFACES SHOULD BE PLACED IN THEIR DESIGNATED FILE
+
+// Interface describing a user object
+interface User {
+  userId: number;
+  email: string;
+  password: string;
+  nameFirst: string;
+  nameLast: string;
+  numSuccessfulLogins: number;
+  previousPasswords: string[];
+  numFailedPasswordsSinceLastLogin: number;
+}
+
+// Interface describing a quiz object
+interface Quiz {
+  quizId: number;
+  quizOwner: number;
+  name: string;
+  timeCreated: number;
+  timeLastEdited: number;
+  description: string;
+}
+
+// Interface describing the dataStore object
+interface DataStore {
+  users: User[];
+  quizzes: Quiz[];
+}
+
+// Interface describing an error object
+interface ErrorObject {
+  error: string;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// EXPORTS /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+export {
+  ErrorObject,
+  User,
+  Quiz,
+  DataStore,
+}
