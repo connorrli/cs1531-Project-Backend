51af16ffdb8329611cc28198aaaa933d80bb3824
Connor Li <z5425430@ad.unsw.edu.au>
2024-03-17 00:42:40 +1100

    Converted auth.js to auth.ts
    Fixed up typing for all functions within

---
+++ src/auth.ts
@@ -0,0 +1,201 @@
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// IMPORTS /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+import { checkDetailsUpdate } from './helpers/auth/userUpdateErrors';
+import { checkUserPasswordUpdate } from './helpers/auth/userPasswordUpdateErrors';
+import { getData, setData } from './dataStore';
+import { invalidRegConditions } from './helpers/auth/registErrors';
+import { error } from './helpers/errors';
+import { authUserIdCheck }  from './helpers/checkForErrors';
+
+import { 
+  DataStore,
+  ErrorObject,
+  User
+} from './interface';
+
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// CONSTANTS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+const NO_ERROR = 0;
+
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// LOCAL INTERFACES /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+interface AdminUserPasswordUpdateReturn { }
+interface AdminUserDetailsUpdateReturn { }
+interface AdminAuthRegisterReturn { authUserId: number; }
+interface AdminAuthLoginReturn { authUserId: number; }
+interface UserDetails {
+  userId: number;
+  name: string;
+  email: string;
+  numSuccessfulLogins: number;
+  numFailedPasswordsSinceLastLogin: number;
+}
+interface AdminUserDetailsReturn { user: UserDetails; }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// FUNCTIONS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+/**
+  * Given details relating to a password change, updates the password of a logged in user.
+  * 
+  * @param {integer} authUserId - Stores user authentication and quiz details
+  * @param {string} oldPassword - Stores the previous user password before the user changed it
+  * @param {string} newPassword - Stores the new user password after the user changed it
+  * 
+  * @returns {empty object} - Returns an empty object to the user
+*/
+function adminUserPasswordUpdate(authUserId: number, oldPassword: string, newPassword: string): AdminUserPasswordUpdateReturn | ErrorObject {
+  const data : DataStore = getData();
+  const userData : User | undefined = data['users'].find(user => user.userId === authUserId);
+
+  const error : ErrorObject | number = checkUserPasswordUpdate(userData, oldPassword, newPassword);
+  if (error !== NO_ERROR) return error;
+
+  userData!.password = newPassword;
+  userData!.previousPasswords.push(oldPassword);
+  setData(data);
+
+  return { };
+}
+
+/**
+  * Registers a user with an email, password, and names, then returns their authUserId value.
+  * 
+  * @param {string} email - Stores the user email as part of the registration
+  * @param {string} password - Stores the user password as part of the registration
+  * @param {string} nameFirst - Stores the user's first name as part of the registration
+  * @param {string} nameLast - Stores the user's second name as part of the registration
+  * 
+  * @returns {object} - Returns the authentication user id
+*/
+function adminAuthRegister(email: string, password: string, nameFirst: string, nameLast: string): AdminAuthRegisterReturn | ErrorObject {
+
+  email = email.toLowerCase();
+
+  const error : ErrorObject | number = invalidRegConditions(email, password, nameFirst, nameLast);
+  if (error !== NO_ERROR) return error as ErrorObject;
+
+  const data : DataStore = getData();
+  const newUser : User = {
+    userId: -1,
+    email: email,
+    password: password,
+    nameFirst: nameFirst,
+    nameLast: nameLast,
+    numSuccessfulLogins: 1,
+    previousPasswords: [],
+    numFailedPasswordsSinceLastLogin: 0,
+  }
+  if (data.users.length === 0) {
+    newUser.userId = 1;
+    data.users.push(newUser);
+  } else {
+    let maxExtantId = 0;
+    for (const extantUser of data.users) {
+      if (extantUser.userId > maxExtantId) {
+        maxExtantId = extantUser.userId;
+      }
+    }
+    newUser.userId = maxExtantId + 1;
+    data.users.push(newUser);
+  }
+
+  return { authUserId: newUser.userId };
+}
+
+/**
+  * Given a registered user's email and password, returns their authUserId value.
+  * 
+  * @param {integer} email - Stores the user email upon login 
+  * @param {integer} password - Stores the user password upon login
+  * 
+  * @returns {object} - Returns the authentication user id
+*/
+function adminAuthLogin(email: string, password: string): AdminAuthLoginReturn | ErrorObject {
+  email = email.toLowerCase();
+  const data : DataStore = getData();
+  const logger : User | undefined = (data.users).find(user => user.email === email);
+  if (logger === undefined) return error.throwError('noEmail');
+  if (password !== logger.password) {
+    logger.numFailedPasswordsSinceLastLogin++;
+    return error.throwError('wrongPassword');
+  }
+  logger.numSuccessfulLogins++;
+
+  return { authUserId: logger.userId };
+}
+
+/**
+  * Given an admin user's authUserId, return details about the user.
+    "name" is the first and last name concatenated with a single space between them.
+  * 
+  * @param {integer} authUserId - Stores user authentication and details about logins
+  * 
+  * @returns {empty object} - Returns the user id number, name, email, count of successful logins and the times where the password has been entered incorrectly
+*/
+function adminUserDetails (authUserId: number): AdminUserDetailsReturn | ErrorObject {
+  if (authUserIdCheck(authUserId) !== NO_ERROR) {
+    return error.throwError('invalidUser');
+  }  
+
+  const data : DataStore = getData();
+  const userData = data.users.find(u => u.userId === authUserId);
+  
+  const user : UserDetails = {
+    userId: userData!.userId,
+    name: userData!.nameFirst + ' ' + userData!.nameLast,
+    email: userData!.email,
+    numSuccessfulLogins: userData!.numSuccessfulLogins,
+    numFailedPasswordsSinceLastLogin: userData!.numFailedPasswordsSinceLastLogin,
+  };
+
+  return { user };
+}
+
+/**
+  * Given an admin user's authUserId and a set of properties, update the properties of this logged in admin user.
+  * 
+  * @param {integer} authUserId - Stores user authentication details after updating properties
+  * @param {integer} email - Stores the user email after updating their properties
+  * @param {string} nameFirst - Stores the first name of the logged in user after updating properties
+  * @param {string} nameLast - Stores the last name of the logged in user after updating properties
+  * 
+  * @returns {empty object} - Returns an empty object to the user
+*/
+function adminUserDetailsUpdate (authUserId: number, email: string, nameFirst: string, nameLast: string): AdminUserDetailsUpdateReturn | ErrorObject {
+
+    // Error check
+    const error : ErrorObject | number = checkDetailsUpdate(authUserId, email, nameFirst, nameLast);
+    if (error !== NO_ERROR) return error;
+
+    // Get and set new details
+    const data : DataStore = getData();
+    const userData : User | undefined = data.users.find(user => user.userId === authUserId);
+    userData!.email = email;
+    userData!.nameFirst = nameFirst;
+    userData!.nameLast = nameLast;
+
+    setData(data);
+
+    return { };
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// EXPORTS /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+export {
+  adminAuthRegister,
+  adminUserDetailsUpdate,
+  adminAuthLogin,
+  adminUserPasswordUpdate,
+  adminUserDetails,
+};
