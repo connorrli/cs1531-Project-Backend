876b21ac81f8a46dca0cc6df418ed20c96bf8c14
connorrli <conesdevelopment@gmail.com>
2024-03-18 15:35:17 +1100

    Completely overhauled tests with HTTP requests
    All tests now rely on HTTP routes instead of
    directly invoking functions. Unsure if works
    until other routes are implemented

--- src/tests/auth/adminUserPasswordUpdate.test.ts
+++ src/tests/auth/adminUserPasswordUpdate.test.ts
@@ -1,48 +1,76 @@
-import { 
-  adminUserPasswordUpdate, 
-  adminAuthRegister, 
-  adminAuthLogin,
-  adminUserDetails, 
-} from "../../auth";
-import { clear } from '../../other';
+import request from 'sync-request-curl';
+import { url, port } from '../../config.json';
+
+const SERVER_URL = `${url}:${port}`;
 
 const ERROR = { error: expect.any(String) };
 
+const clearRequest = () => {
+  const response = request('DELETE', SERVER_URL + 'v1/clear', { qs: { } });
+  return JSON.parse(response.body.toString());
+}
+
+const loginRequest = (email: string, password: string) => {
+  const response = request('POST', SERVER_URL + 'v1/admin/auth/login', { json: { email, password } });
+  return JSON.parse(response.body.toString());
+}
+
+const responseGet = (token: string, oldPassword: string, newPassword: string) => {
+  const response = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword, newPassword } });
+  return JSON.parse(response.body.toString());
+}
+
+const userDetailsRequest = (token: string) => {
+  const response = request('GET', SERVER_URL + '/v1/admin/user/details', { qs: { token } });
+  return JSON.parse(response.body.toString());
+}
+
+const userCreateRequest = (email: string, password: string, nameFirst: string, nameLast: string) => {
+  const response = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: { email, password, nameFirst, nameLast } });
+  return JSON.parse(response.body.toString());
+}
+
 describe('Testing adminUserPasswordUpdate function:', () => {
-  let user1;
-  let user1_id : number;
-  let user2_id : number;
-  const password = 'Password123';
+  const INIT_VALID_EMAIL = 'z000000@ad.unsw.edu.au';
+  const INIT_VALID_PASSWORD = 'Password123';
+  let userToken1 : string;
+  let userToken2 : string;
   beforeEach(() => {
-    clear();
-    user1 = adminAuthRegister('z000000@ad.unsw.edu.au', password,'John','Doe');
-    if ('authUserId' in user1) user1_id = user1.authUserId;
-    user2_id = -1;
+    clearRequest();
+    userToken1 = userCreateRequest(INIT_VALID_EMAIL, INIT_VALID_PASSWORD, 'John', 'Doe').token;
+    userToken2 = '';
   });
   test.each([
-    ['Valid Password Change', password, 'Password321', {}, 2],
+    ['Valid Password Change', INIT_VALID_PASSWORD, 'Password321', {}, 2],
     ['Old Password Incorrect', 'Password321', 'password1234', ERROR, 1],
-    ['New Password is Old Password', password, password, ERROR, 2],
-    ['New Password Length < 8', password, 'Pass1', ERROR, 1],
-    ['New Password No Numbers', password, 'Password', ERROR, 1],
-    ['New Password No Letters', password, '12345678', ERROR, 1],
+    ['New Password is Old Password', INIT_VALID_PASSWORD, INIT_VALID_PASSWORD, ERROR, 2],
+    ['New Password Length < 8', INIT_VALID_PASSWORD, 'Pass1', ERROR, 1],
+    ['New Password No Numbers', INIT_VALID_PASSWORD, 'Password', ERROR, 1],
+    ['New Password No Letters', INIT_VALID_PASSWORD, '12345678', ERROR, 1],
   ])('Testing %s:', (testName, oldPassword, newPassword, expectedReturn1, expectedReturn2) => {
-    expect(adminUserPasswordUpdate(user1_id, oldPassword, newPassword)).toStrictEqual(expectedReturn1);
-    // Check that new password either worked or didn't work depending on if it changed or not
-    adminAuthLogin('z000000@ad.unsw.edu.au', newPassword);
-    const userDetails = adminUserDetails(user1_id);
-    if ('user' in userDetails) expect(userDetails.user.numSuccessfulLogins).toStrictEqual(expectedReturn2);
+    const response1 = responseGet(userToken1, oldPassword, newPassword);
+
+    expect(response1).toStrictEqual(expectedReturn1);
+
+    loginRequest(INIT_VALID_EMAIL, newPassword);
+    const response2 = userDetailsRequest(userToken1);
+    expect(response2.user.numSuccessfulLogins).toStrictEqual(expectedReturn2);
   });
   test('User Doesn\'t Exist', () => {
     const newPassword = 'password321';
-    expect(adminUserPasswordUpdate(user2_id, password, newPassword)).toStrictEqual(ERROR);
+    const response = responseGet(userToken2, INIT_VALID_PASSWORD, newPassword);
+    expect(response).toStrictEqual(ERROR);
   });
   test('New Password is a Previous Password', () => {
-    const newPassword = 'password321';
-    adminUserPasswordUpdate(user1_id, password, newPassword);
-    expect(adminUserPasswordUpdate(user1_id, 'Password321', password)).toStrictEqual(ERROR);
-    adminAuthLogin('z000000@ad.unsw.edu.au', password);
-    const userDetails = adminUserDetails(user1_id);
-    if ('user' in userDetails) expect(userDetails.user.numSuccessfulLogins).toStrictEqual(1);
+    const newPassword = 'Password12345';
+    responseGet(userToken1, INIT_VALID_PASSWORD, newPassword);
+
+    const response1 = responseGet(userToken1, newPassword, INIT_VALID_PASSWORD);
+    expect(response1).toStrictEqual(ERROR);
+
+    loginRequest(INIT_VALID_EMAIL, INIT_VALID_PASSWORD);
+    const response2 = userDetailsRequest(userToken1);
+
+    expect(response2.user.numSuccessfulLogins).toStrictEqual(1);
   });
 });
