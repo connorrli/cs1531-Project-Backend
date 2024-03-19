7eb5c2c8e4623939e4b9efda3c232104160403c6
Zane <z5477050@ad.unsw.edu.au>
2024-03-19 15:56:55 +1100

    Updated all tests to work with typeScript

--- src/tests/quiz/adminQuizRemove.test.ts
+++ src/tests/quiz/adminQuizRemove.test.ts
@@ -13,40 +13,62 @@ describe('adminQuizRemove function tests', () => {
     })
 
     test('Should remove a quiz owned by the user', () => {
-        const { authUserId } = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
-        const { quizId } = adminQuizCreate(authUserId, 'Test Quiz', 'This is a test quiz');
-        expect(adminQuizInfo(authUserId, quizId)).not.toHaveProperty("error");
-        const result = adminQuizRemove(authUserId, quizId);
-        expect(result).toEqual({});
-        expect(adminQuizInfo(authUserId, quizId)).toHaveProperty("error");
+        const test1U = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
+        if ('authUserId' in test1U) {
+            const test1Q = adminQuizCreate(test1U.authUserId, 'Test Quiz', 'This is a test quiz');
+            if ('quizId' in test1Q){
+                expect(adminQuizInfo(test1U.authUserId, test1Q.quizId)).not.toHaveProperty("error");
+                const result = adminQuizRemove(test1U.authUserId, test1Q.quizId);
+                expect(result).toStrictEqual({});
+                expect(adminQuizInfo(test1U.authUserId, test1Q.quizId)).toHaveProperty("error");
+            }
+        }    
     });
 
     test('Should return an error when authUserId is invalid', () => {
-        const { authUserId } = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
-        const { quizId } = adminQuizCreate(authUserId, 'Test Quiz', 'This is a test quiz');
-        const result = adminQuizRemove(authUserId + 1, quizId);
-        expect(result).toEqual(ERROR);
+        const test2U = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
+        if ('authUserId' in test2U) {
+            const test2Q = adminQuizCreate(test2U.authUserId, 'Test Quiz', 'This is a test quiz');
+            if ('quizId' in test2Q){
+                const result = adminQuizRemove(test2U.authUserId + 1, test2Q.quizId);
+                expect(result).toStrictEqual(ERROR);
+            }
+        }
     });
 
     test('Should return an error when quizId does not refer to a valid quiz', () => {
-        const { authUserId } = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
-        const result = adminQuizRemove(authUserId, 123);
-        expect(result).toEqual(ERROR);
+        const test3U = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
+        if ('authUserId' in test3U) {
+            const result = adminQuizRemove(test3U.authUserId, 123);
+            expect(result).toStrictEqual(ERROR);
+        }
     });
     
     test('Should return an error when quizId does not refer to a quiz that the user owns', () => {
-        const { authUserId: userId1 } = adminAuthRegister('test1@example.com', 'password123', 'John', 'Doe');
-        const { authUserId: userId2 } = adminAuthRegister('test2@example.com', 'password1232', 'Jane', 'Smith');
-        const { quizId } = adminQuizCreate(userId1, 'Test Quiz', 'This is a test quiz');
-        const result = adminQuizRemove(userId2, quizId);
-        expect(result).toEqual(ERROR);
+        const test4U1 = adminAuthRegister('test1@example.com', 'password123', 'John', 'Doe');
+        if ('authUserId' in test4U1) {
+            const test4U2 = adminAuthRegister('test2@example.com', 'password1232', 'Jane', 'Smith');
+            if ('authUserId' in test4U2) {
+                const test4Q = adminQuizCreate(test4U1.authUserId, 'Test Quiz', 'This is a test quiz');
+                if ('quizId' in test4Q){
+                    const result = adminQuizRemove(test4U2.authUserId, test4Q.quizId);
+                    expect(result).toStrictEqual(ERROR);
+                }
+            }
+        }
     });
 
     test('Should NOT return an error when quizId DOES refer to a quiz that the user owns', () => {
-        const { authUserId: userId1 } = adminAuthRegister('test1@example.com', 'password123', 'John', 'Doe');
-        const { authUserId: userId2 } = adminAuthRegister('test2@example.com', 'password1232', 'Jane', 'Smith');
-        const { quizId } = adminQuizCreate(userId1, 'Test Quiz', 'This is a test quiz');
-        const result = adminQuizRemove(userId1, quizId);
-        expect(result).not.toHaveProperty('error');
+        const test5U1 = adminAuthRegister('test1@example.com', 'password123', 'John', 'Doe');
+        if ('authUserId' in test5U1) {
+            const test5U2 = adminAuthRegister('test2@example.com', 'password1232', 'Jane', 'Smith');
+            if ('authUserId' in test5U2) {
+                const test5Q = adminQuizCreate(test5U1.authUserId, 'Test Quiz', 'This is a test quiz');
+                if ('quizId' in test5Q){
+                    const result = adminQuizRemove(test5U1.authUserId, test5Q.quizId);
+                    expect(result).not.toStrictEqual(ERROR);  
+                }
+            }
+        }
     });
 });
