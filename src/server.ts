01c380d22e6e814db97b66d69234681c437cd475
z5600055 <z5600055@vx22.orchestra.cse.unsw.EDU.AU>
2024-03-21 17:04:37 +1100

    Fixed merge conflicts

--- src/server.ts
+++ src/server.ts
@@ -169,6 +169,44 @@ app.delete('/v1/clear', (req: Request, res: Response) => {
   res.json(response);
 });
 
+// adminQuizInfo Route
+app.get('/v1/admin/quiz/:quizid', (req: Request, res: Response) => {
+  const quizId = parseInt(req.params.quizid);
+  const token = req.query.token as string;
+  if (!token) {
+    return res.status(401).json({ error: "Token is missing" });
+  }
+  const session = getSession(token);
+  if (!session || !('userId' in session)) {
+    return res.status(401).json({ error: "Invalid session" });
+  }
+  const userId = session.userId;
+  const response = adminQuizInfo(userId, quizId);
+  if ('error' in response) {
+    return res.status(403).json(response);
+  }
+  return res.status(200).json(response);
+});
+
+// adminQuizNameUpdate Route
+app.put('/v1/admin/quiz/:quizid/name', (req: Request, res: Response) => {
+  const quizId = parseInt(req.params.quizid);
+  const name = req.body.name;
+  const token = req.query.token as string; // Does every function come with a token in the query???
+  if (!token) {
+    return res.status(401).json({ error: "Token is missing" });
+  }
+  const session = getSession(token);
+  if (!session || !('userId' in session)) {
+    return res.status(401).json({ error: "Invalid session" });
+  }
+  const userId = session.userId;
+  const response = adminQuizNameUpdate(userId, quizId, name);
+  if ('error' in response) {
+    return res.status(403).json(response);
+  }
+});
+
 // ====================================================================
 //  ================= WORK IS DONE ABOVE THIS LINE ===================
 // ====================================================================
