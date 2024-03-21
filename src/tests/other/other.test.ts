892f46b3cc0000a095c961a9f2909d23ac08c299
Zhitan Wu <z5600055@ad.unsw.edu.au>
2024-03-21 21:50:49 +1100

    Updated other.test.ts with a lot of comments as documentation

--- src/tests/other/other.test.ts
+++ src/tests/other/other.test.ts
@@ -1,3 +1,4 @@
+// Import statements, initiating the server and ensuring that the database is empty before each test
 import request from 'sync-request-curl';
 import { url, port } from '../../config.json';
 
@@ -7,31 +8,37 @@ beforeEach(() => {
     request('DELETE', SERVER_URL + '/v1/clear', {qs: {} });
 });
 
+// 'ClearRequest' function
 const clearRequest = () => {
     const response = request('DELETE', SERVER_URL + `/v1/clear`, { qs: {} });
     return JSON.parse(response.body.toString());
 }
 
+// 'userDetailsRequest' function
 const userDetailsRequest = (token: string) => {
     const response = request('GET', SERVER_URL + `/v1/admin/user/details`, { qs: { token } });
     return JSON.parse(response.body.toString());
 }
 
+// 'quizListRequest' function
 const quizListRequest = (token: string) => {
     const response = request('GET', SERVER_URL + `/v1/admin/quiz/list`, { qs: { token } });
     return JSON.parse(response.body.toString());
 }
 
+// 'userCreateRequest' function
 const userCreateRequest = (email: string, password: string, nameFirst: string, nameLast: string) => {
     const response = request('POST', SERVER_URL + `/v1/admin/auth/register`, { json: { email, password, nameFirst, nameLast } });
     return JSON.parse(response.body.toString());
 }
 
+// 'quizCreateRequest' function
 const quizCreateRequest = (token: string, name: string, description: string) => {
     const response = request('POST', SERVER_URL + `/v1/admin/quiz`, { json: { token, name, description } });
     return JSON.parse(response.body.toString());
 }
 
+// General clearing of user data, test
 test('Should clear user data', () => {
     const user1 = userCreateRequest('anotheruser@genericemail.com', 'password123', 'Another', 'User');
     clearRequest();
@@ -39,6 +46,7 @@ test('Should clear user data', () => {
     expect(userData.error).toBeDefined();
 });
 
+// General clearing of quiz data, test
 test('Should clear quiz data', () => {
     clearRequest();
     const user1 = userCreateRequest('anotheruser@genericemail.com', 'password123', 'Another', 'User');
@@ -53,6 +61,7 @@ test('Should clear quiz data', () => {
     expect(quizzesAfterClear.error).toBeDefined();
 });
 
+// Formal test for the function to delete quiz data in the datastore
 test('Should actually delete the quiz data in the datastore', () => {
     const ownerUser = userCreateRequest('owner@example.com', 'password123', 'Owner', 'User');
     const quizId = quizCreateRequest(ownerUser.token, 'Math Quiz', 'This is a math quiz.');
