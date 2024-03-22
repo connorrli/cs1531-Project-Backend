b547bb09de906fcb2bd458cc2af6ba98ec04155d
Connor Li <z5425430@ad.unsw.edu.au>
2024-03-22 16:06:07 +1100

    Adds HTTP wrapper fn for adminUserDetailsUpdate

--- src/tests/requests.ts
+++ src/tests/requests.ts
@@ -23,6 +23,11 @@ export const userDetailsRequest = (token: string) => {
   return JSON.parse(response.body.toString());
 }
 
+export const userDetailsUpdateRequest = (token: string, email: string, nameFirst: string, nameLast: string) => {
+  const response = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email, nameFirst, nameLast } });
+  return JSON.parse(response.body.toString());
+}
+
 export const userCreateRequest = (email: string, password: string, nameFirst: string, nameLast: string) => {
   const response = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: { email, password, nameFirst, nameLast } });
   return JSON.parse(response.body.toString());
