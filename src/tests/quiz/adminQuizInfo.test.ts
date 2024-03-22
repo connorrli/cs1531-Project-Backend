11684711e8eb0dd9e69d6394351c0b7ef88e8f9e
Holly Shephard <z5476845@ad.unsw.edu.au>
2024-03-22 15:05:02 +1100

    made adminQuizInfo test use http requests/responses

--- src/tests/quiz/adminQuizInfo.test.ts
+++ src/tests/quiz/adminQuizInfo.test.ts
@@ -1,5 +1,3 @@
-import { adminQuizCreate, adminQuizInfo } from "../../quiz";
-import { clear } from "../../other";
 import { url, port } from '../../config.json';
 import request from 'sync-request-curl';
 
@@ -10,19 +8,26 @@ const adminAuthRegister = (email: string, password: string, nameFirst: string, n
     const result = request('POST', SERVER_URL + '/v1/admin/auth/register', {json: {nameFirst, nameLast, email, password}});
     return JSON.parse(result.body.toString());
 }
+const adminQuizCreate = (token: string, name: string, description: string) => {
+    const result = request('POST', SERVER_URL + '/v1/admin/quiz', { json: {token, name, description} });
+    return JSON.parse(result.body.toString());
+}
+const adminQuizInfo = (token: string, quizId: number) => {
+    const result = request('GET', SERVER_URL + '/v1/admin/quiz/' + quizId.toString(), { qs: { token }});
+    return JSON.parse(result.body.toString());
+}
+
+beforeEach(() => {
+    request('DELETE', SERVER_URL + '/v1/clear', { qs: {} });
+});
 
 describe('adminQuizInfo function tests', () => {
-
-    beforeEach(() => {
-        clear();
-    })
-
     test('Returns quiz information for a valid quiz owned by the user', () => {
         const authUser = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
-        if ('authUserId' in authUser) {
-            const quiz = adminQuizCreate(authUser.authUserId, 'Test Quiz', 'This is a test quiz');
+        if ('token' in authUser) {
+            const quiz = adminQuizCreate(authUser.token, 'Test Quiz', 'This is a test quiz');
             if ('quizId' in quiz) {
-                const quizInfo = adminQuizInfo(authUser.authUserId, quiz.quizId);
+                const quizInfo = adminQuizInfo(authUser.token, quiz.quizId);
                 expect(quizInfo).toStrictEqual({
                     quizId: quiz.quizId,
                     name: 'Test Quiz',
@@ -34,26 +39,26 @@ describe('adminQuizInfo function tests', () => {
         }
     });
 
-    test('Returns an error when authUserId is not a valid user', () => {
-        const quizInfo = adminQuizInfo(42, 42);
+    test('Returns an error when token is not a valid user', () => {
+        const quizInfo = adminQuizInfo("42", 42);
         expect(quizInfo).toEqual(ERROR);
     });
 
     test('Returns an error when quizId is not a valid quiz', () => {
         const authUser = adminAuthRegister('test@example.com', 'password123', 'John', 'Doe');
-        if ('authUserId' in authUser) {
-            const quizInfo = adminQuizInfo(authUser.authUserId, 42);
+        if ('token' in authUser) {
+            const quizInfo = adminQuizInfo(authUser.token, 42);
             expect(quizInfo).toEqual(ERROR);
         }
     });
 
-    test('Returns an error when quizId is not owned by authUserId', () => {
+    test('Returns an error when quizId is not owned by token', () => {
         const authUser1 = adminAuthRegister('user1@example.com', 'password231', 'First', 'User');
         const authUser2 = adminAuthRegister('user2@example.com', 'password123', 'Second', 'User');
-        if ('authUserId' in authUser1 && 'authUserId' in authUser2) {
-            const quiz = adminQuizCreate(authUser1.authUserId, 'User 1 Quiz', 'This is a quiz created by user 1');
+        if ('token' in authUser1 && 'token' in authUser2) {
+            const quiz = adminQuizCreate(authUser1.token, 'User 1 Quiz', 'This is a quiz created by user 1');
             if ('quizId' in quiz) {
-                const quizInfo = adminQuizInfo(authUser2.authUserId, quiz.quizId);
+                const quizInfo = adminQuizInfo(authUser2.token, quiz.quizId);
                 expect(quizInfo).toEqual(ERROR);
             }
         }
