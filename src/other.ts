532dd8e502607b8035c7b04f44674d0e1b61bce5
Zhitan Wu <z5600055@ad.unsw.edu.au>
2024-03-24 22:28:07 +1100

    Updated src/other.ts with clearTrash() temporary implementation

--- src/other.ts
+++ src/other.ts
@@ -5,10 +5,36 @@
 interface ClearReturn { }
 
 import { getData, setData } from './dataStore';
+import { getTrash, setTrash } from './trash';
+
 
 function clear(): ClearReturn {
   setData({ users: [], quizzes: [], sessions: [] });
   return {};
 }
 
-export { clear };
+/* function clearTrash(userId: number, token: string, quizIds: string): ClearReturn {
+  const trash = getTrash();
+  for (let quizIndex in trash.quizzes) {
+    if (trash.quizzes[quizIndex].quizOwner === userId && quizIds.includes(trash.quizzes[quizIndex].userId)) {
+      trash.quizzes.splice(quizIndex, 1);
+    }
+  }
+  setTrash(trash);
+} */
+
+function clearTrash(userId: number, token: string, quizIds: string): void {
+
+  const trash = getTrash();
+
+  for (let quizIndex = 0; quizIndex < trash.quizzes.length; quizIndex++) {
+    if (trash.quizzes[quizIndex].quizOwner === userId && quizIds.includes(String(trash.quizzes[quizIndex].quizId))) {
+      trash.quizzes.splice(quizIndex, 1);
+      quizIndex--;
+    }
+  }
+  
+  setTrash(trash);
+}
+
+export { clear, clearTrash};
