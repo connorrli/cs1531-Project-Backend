76d909e642feacb8623d9b698291de3a168e1a18
Connor Li <z5425430@ad.unsw.edu.au>
2024-04-10 21:34:31 +1000

    Mimics tests for adminQuizDescriptionUpdateV2
    Changes all request functions to be new V2 version

--- src/tests/quiz/adminQuizDescriptionUpdate.test.ts
+++ src/tests/quiz/adminQuizDescriptionUpdate.test.ts
@@ -1,6 +1,7 @@
 
 import request from 'sync-request-curl';
 import { url, port } from '../../config.json';
+import { quizCreateRequestV2, quizDescriptionUpdateRequestV2, quizInfoRequestV2 } from '../requests';
 const ERROR = { error: expect.any(String) };
 const SERVER_URL = `${url}:${port}`;
 
@@ -32,7 +33,7 @@ beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { qs: {} });
 });
 
-describe('quizDescUpdReq function tests', () => {
+describe('quizDescriptionUpdate function tests', () => {
   test('Should return an error when quizId does not refer to a valid quiz', () => {
     const authUserTok = authRegisterReq('test@example.com', 'password123', 'John', 'Doe');
     const quizId = quizCreateReq(authUserTok.token, 'Test Quiz', 'This is a test quiz');
@@ -81,6 +82,59 @@ describe('quizDescUpdReq function tests', () => {
     const authUserTok = authRegisterReq('test@example.com', 'password976', 'Peter', 'Kim');
     const quizId = quizCreateReq(authUserTok.token, 'Test Quiz', 'This is a test quiz');
     const result = quizDescUpdReq(authUserTok.token + '1', quizId.quizId, '');
+    expect(result).toEqual(ERROR);
+  });
+});
+
+describe('quizDescriptionUpdateV2 function tests', () => {
+  test('Should return an error when quizId does not refer to a valid quiz', () => {
+    const authUserTok = authRegisterReq('test@example.com', 'password123', 'John', 'Doe');
+    const quizId = quizCreateRequestV2(authUserTok.token, 'Test Quiz', 'This is a test quiz');
+    quizDescriptionUpdateRequestV2(authUserTok.token, quizId.quizId, 'Updated Test Quiz Description');
+
+    const result = quizDescriptionUpdateRequestV2(authUserTok.token, quizId.quizId + 1, '');
+    expect(result.error).toEqual(expect.any(String));
+  });
+
+  test('Should return an error when quizId does not refer to a quiz that the user owns', () => {
+    const userId1 = authRegisterReq('test1@example.com', 'password643', 'John', 'Doe');
+    const userId2 = authRegisterReq('test2@example.com', 'password292', 'Jane', 'Smith');
+    const quizId = quizCreateRequestV2(userId1.token, 'Test Quiz', 'This is a test quiz');
+
+    quizDescriptionUpdateRequestV2(userId1.token, quizId.quizId, 'Updated Test Quiz Description');
+
+    const result = quizDescriptionUpdateRequestV2(userId2.token, quizId.quizId, '');
+    expect(result.error).toEqual(expect.any(String));
+  });
+
+  test('Should return an error when the description is more than 100 characters in length', () => {
+    const authUserTok = authRegisterReq('test@example.com', 'password433', 'John', 'Doe');
+    const quizId = quizCreateRequestV2(authUserTok.token, 'Test Quiz', 'This is a test quiz');
+
+    const newDescription = 'This is a description longer than 100 characters. ' + '...'.repeat(50);
+    const result = quizDescriptionUpdateRequestV2(authUserTok.token, quizId.quizId, newDescription);
+
+    expect(result.error).toEqual(expect.any(String));
+  });
+
+  // This is the only success case here. All the others are error cases.
+  test('Should update the quiz description successfully', () => {
+    const authUserTok = authRegisterReq('test@example.com', 'password123', 'John', 'Doe');
+    const quizId = quizCreateRequestV2(authUserTok.token, 'Test Quiz', 'This is a test quiz');
+
+    const newDescription = 'This is an updated test quiz description';
+    quizDescriptionUpdateRequestV2(authUserTok.token, quizId.quizId, newDescription);
+
+    const quizInfo = quizInfoRequestV2(authUserTok.token, quizId.quizId);
+
+    expect(quizInfo.error).toBeUndefined();
+    expect(quizInfo.description).toBe(newDescription);
+  });
+
+  test('Should return an error when authUserId is invalid', () => {
+    const authUserTok = authRegisterReq('test@example.com', 'password976', 'Peter', 'Kim');
+    const quizId = quizCreateRequestV2(authUserTok.token, 'Test Quiz', 'This is a test quiz');
+    const result = quizDescriptionUpdateRequestV2(authUserTok.token + '1', quizId.quizId, '');
     expect(result).toEqual(ERROR);
   });
 });
