e326eccaad58f710c999a4e8c56c775098d5d3b9
Zhitan Wu <z5600055@ad.unsw.edu.au>
2024-04-13 10:03:53 +1000

    Updated files

---
+++ src/tests/quiz/adminQuizThumbnailUpdate.ts
@@ -0,0 +1,6 @@
+
+import request from 'sync-request-curl';
+import { url, port } from '../../config.json';
+import { userCreateRequest, quizCreateRequestV2 } from '../requests';
+const ERROR = { error: expect.any(String) };
+const SERVER_URL = `${url}:${port}`;
