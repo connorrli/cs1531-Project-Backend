bf87505a14bdeba8e595388fdc4fbf6c871d8f50
Zane <z5477050@ad.unsw.edu.au>
2024-04-16 10:28:48 +1000

    Finished writing all tests for adminQuizSessionResults

--- src/tests/quiz/adminQuizSessionResults.test.ts
+++ src/tests/quiz/adminQuizSessionResults.test.ts
@@ -1,5 +1,6 @@
 import { QuestionBodyV2 } from '../../interface';
 import { clearRequest, quizSessionResultsRequest, quizSessionStartRequest, userCreateRequest, quizCreateRequestV2, questionCreateRequestV2, quizSessionStatusRequest } from '../requests';
+import { playerJoinRequest, quizSessionStateUpdateRequest, playerQuestionInfoRequest, playerSubmitRequest, playerQuestionResultsRequest } from '../requests';
 
 const ERROR_RESPONSE = { error: expect.any(String) };
 
@@ -13,7 +14,7 @@ enum States {
   END = 'END'
 }
 
-describe('Testing adminQuizSessionResults function:', () => {
+describe('Testing adminQuizSessionResults function ERROR CASES:', () => {
   let userToken: string;
   let quizId: number;
   let sessionId: number;
@@ -93,12 +94,6 @@ describe('Testing adminQuizSessionResults function:', () => {
     sessionId = quizSessionStartRequest(userToken, quizId, 1).sessionId;
   });
 
-  test('Success Case: Get final results for a completed quiz session', () => {
-    const response = quizSessionResultsRequest(userToken, quizId, sessionId);
-    expect(response).toHaveProperty('usersRankedByScore');
-    expect(response).toHaveProperty('questionResults');
-  });
-
   test('Error Case: Get results with invalid sessionId', () => {
     const invalidSessionId = sessionId + 1;
     const response = quizSessionResultsRequest(userToken, quizId, invalidSessionId);
@@ -129,4 +124,91 @@ describe('Testing adminQuizSessionResults function:', () => {
     expect(response).toEqual(ERROR_RESPONSE);
   });
 
+});
+
+
+describe('Testing adminQuizSessionResults function SUCCESS CASE:', () => {
+  let john: { token: string };
+  let quiz: { quizId: number };
+  let session: { sessionId: number };
+  let player: { playerId: number };
+  let player2: { playerId: number };
+  let answers1: { answer1: number, answer2: number };
+  let answers2: { answer1: number, answer2: number };
+
+  beforeEach(() => {
+    clearRequest();
+    john = userCreateRequest('johndoe@gmail.com', 'passw1234', 'john', 'doe');
+    quiz = quizCreateRequestV2(john.token, 'Johns quiz', '');
+    questionCreateRequestV2(john.token, quiz.quizId, {
+      question: 'What course is this?',
+      duration: 10,
+      points: 5,
+      answers: [
+        { answer: 'COMP1531', correct: true },
+        { answer: 'MATH1081', correct: false }
+      ],
+      thumbnailUrl: 'http://google.com.jpeg'
+    });
+    questionCreateRequestV2(john.token, quiz.quizId, {
+      question: 'what is F15ABOOST getting in this course',
+      duration: 10,
+      points: 5,
+      answers: [
+        { answer: 'HD BABY', correct: true },
+        { answer: 'fail', correct: false }
+      ],
+      thumbnailUrl: 'http://google.com.jpeg'
+    });
+    session = quizSessionStartRequest(john.token, quiz.quizId, 5);
+    player = playerJoinRequest('jimmy', session.sessionId);
+    player2 = playerJoinRequest('jimmytwo', session.sessionId);
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'NEXT_QUESTION');
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'SKIP_COUNTDOWN');
+    const answers1Array = playerQuestionInfoRequest(player.playerId, 1).answers;
+    answers1 = { answer1: answers1Array[0].answerId, answer2: answers1Array[1].answerId };
+    playerSubmitRequest([answers1.answer1], player.playerId, 1);
+    playerSubmitRequest([answers1.answer2], player2.playerId, 1);
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'GO_TO_ANSWER');
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'NEXT_QUESTION');
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'SKIP_COUNTDOWN');
+    const answers2Array = playerQuestionInfoRequest(player.playerId, 2).answers;
+    answers2 = { answer1: answers2Array[0].answerId, answer2: answers2Array[1].answerId };
+    playerSubmitRequest([answers2.answer1], player.playerId, 2);
+    playerSubmitRequest([answers2.answer2], player2.playerId, 2);
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'GO_TO_ANSWER');
+    quizSessionStateUpdateRequest(john.token, quiz.quizId, session.sessionId, 'GO_TO_FINAL_RESULTS');
+  });
+  test('Expected behaviour', () => {
+    expect(quizSessionResultsRequest(john.token, quiz.quizId, session.sessionId)).toStrictEqual({
+      usersRankedByScore: [
+        {
+          name: 'jimmy',
+          score: expect.any(Number)
+        },
+        {
+          name: 'jimmytwo',
+          score: expect.any(Number)
+        }
+      ],
+      questionResults: [
+        {
+          questionId: expect.any(Number),
+          playersCorrectList: [
+            'jimmy'
+          ],
+          averageAnswerTime: expect.any(Number),
+          percentageCorrect: 50
+        },
+        {
+          questionId: expect.any(Number),
+          playersCorrectList: [
+            'jimmy'
+          ],
+          averageAnswerTime: expect.any(Number),
+          percentageCorrect: 50
+        }
+      ]
+    });
+  });
 });
